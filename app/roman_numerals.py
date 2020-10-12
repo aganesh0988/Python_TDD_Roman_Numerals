9ab91b604a192e21bb969078a7b92402a8a97c83
Andrea <aganesh0988@gmail.com>
2020-10-12 16:57:44 -0500

    added roman numerals 1-8

---
+++ app/roman_numerals.py
@@ -0,0 +1,17 @@
+def parse(rn):
+    if rn == "I":
+        return 1
+    elif rn == "II":
+        return 2
+    elif rn == "III":
+        return 3
+    elif rn == "IV":
+        return 4
+    elif rn == "V":
+        return 5
+    elif rn == "VI":
+        return 6
+    elif rn == "VII":
+        return 7
+    elif rn == "VIII":
+        return 8
